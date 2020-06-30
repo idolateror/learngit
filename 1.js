5288ee0590f44ab1099fbcbf2c8b9b56e6eb5b12
liuya <liuya91@icourt.cc>
2020-06-30 14:00:24 +0800

    read-1

--- 1.js
+++ 1.js
@@ -1,3 +1,14 @@
+/**
+ * @desc 1.初始化仓库和提交操作
+ * git init
+ * git add .
+ * git commit - m 'remark'
+ * 
+ * @desc 2.查看状态
+ * git status
+ * git log --pretty=oneline
+ */
+
 // 1
 var text = 1
 // 添加第2条
